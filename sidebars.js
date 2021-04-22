56dd99571909a93fae5cbca6fda8516ee0e6a65a
Bogdan Kovtun <psirex48@gmail.com>
2021-04-23 01:03:57 +0300

    Add guides section

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,15 @@ module.exports = {
     'lido-dao',
     {
       type: 'category',
+      label: 'Guides',
+      items: [
+        'guides/node-operator-manual',
+        'guides/oracle-operator-manual',
+        'guides/multisig-deployment',
+      ],
+    },
+    {
+      type: 'category',
       label: 'Contracts',
       items: [
         'contracts/lido',
