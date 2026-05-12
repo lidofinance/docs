247990795812edb0176ef094e584f6bfe156068a
Karina <karinamaulitova@gmail.com>
2026-05-12 18:40:06 +0200

    fix: replace KaTeX CDN stylesheet with local import in custom.css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,14 +17,6 @@ module.exports = async function createConfigAsync() {
       },
     },
     themes: ['@docusaurus/theme-mermaid'],
-    stylesheets: [
-      {
-        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-        type: 'text/css',
-        integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-        crossorigin: 'anonymous',
-      },
-    ],
     themeConfig: {
       prism: {
         additionalLanguages: ['solidity'],
